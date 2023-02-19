ee5b292d98ac6f77a408ac3f7efcd4f5f65f6796
Moamin <moamin.salamah@gmail.com>
2023-02-19 15:39:49 +0100

    re-setting up the project folders

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,32 @@
 <template>
   <div id="app">
-    <Navbar />
-    <Sidebar />
-    <b-container>
-      <MainPage />
-    </b-container>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import MainPage from "./components/MainPage.vue";
-import Navbar from "./components/Navbar.vue";
-import Sidebar from "./components/Sidebar.vue";
-export default {
-  name: "App",
-  components: { Navbar, Sidebar, MainPage },
-};
-</script>
-
 <style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
-  color: #444444;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
