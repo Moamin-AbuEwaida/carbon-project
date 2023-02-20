5e616c3cab299121dd0ac125598d0b86cb2365f9
Moamin <moamin.salamah@gmail.com>
2023-02-20 10:59:25 +0100

    adding dist folder

---
+++ dist/js/961.ecf17d79.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkcarbon_project"]=self["webpackChunkcarbon_project"]||[]).push([[961],{8961:function(n,s,t){t.r(s),t.d(s,{default:function(){return l}});var a=function(){var n=this;n._self._c;return n._m(0)},e=[function(){var n=this,s=n._self._c;return s("div",[s("div",{staticClass:"analysis"},[s("div",[n._v("analysis")])])])}],r={name:"AnalysisView"},i=r,c=t(1001),u=(0,c.Z)(i,a,e,!1,null,"0bb47972",null),l=u.exports}}]);
+//# sourceMappingURL=961.ecf17d79.js.map
